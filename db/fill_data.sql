a8afe004b741c2d183d1756f391bd35873a5b6d4
Andrei Belskii <mrandrewer@yandex.ru>
2024-12-04 22:32:34 +0300

    Add tests initial data

--- db/fill_data.sql
+++ db/fill_data.sql
@@ -3,3 +3,9 @@ insert into teachers (full_name, phone, email) values
 ('Петров Виктор Александрович', '+79116543322', 'petrov-viktor@mail.ru'),
 ('Серебрякова Инна Леонидовна', '+79045878432', null),
 ('Коноплева Валерия Викторовна', null, 'konopleva1965@mail.ru');
+
+insert into tests (teacher_id, name, content) values
+(1, 'Задача 1', 'Создать игру кликер до 27.11.'),
+(2, 'Задача 2', 'Создать приложение с подключением к БД.\nДобавить в приложение CRUD для таблицы Teachers на базе QSqlQueryModel до 2.12.'),
+(3, 'Задача 3', 'Добавить в приложение CRUD для таблицы Test на базе QSqlTableModel до 9.12.'),
+(3, 'Задача 4', 'Добавить в приложение CRUD для таблицы Test на базе QSqlRelationalTableModel до 16.12.');
